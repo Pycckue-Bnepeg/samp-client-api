b66c1a37ca4c34fb0f05fd731d2d4451fc975466
ZOTTCE <zottce@gmail.com>
2021-10-25 13:35:10 +0300

    set_render_state

--- src/gta/rw.rs
+++ src/gta/rw.rs
@@ -4,9 +4,19 @@ pub mod rwplcore;
 
 // RpClump* RpClumpForAllAtomics(RpClump* clump, RpAtomicCallBack callback, void* pData); // 0x749B70
 // RpGeometry* RpGeometryForAllMaterials(RpGeometry* geometry, RpMaterialCallBack fpCallBack, void* pData); // 0x74C790
+// RwGlobals *&RwEngineInstance = *(RwGlobals **)0xC97B24;
+static mut RW_ENGINE_INSTANCE: *mut *mut rwplcore::RwGlobals = 0xC97B24 as *mut *mut _;
 
-type RpClumpForAllAtomics = extern "C" fn(*mut rpworld::RpClump, rpworld::RpAtomicCallBack, *mut std::os::raw::c_void) -> *mut rpworld::RpClump;
-type RpGeometryForAllMaterials = extern "C" fn(*mut rpworld::RpGeometry, rpworld::RpMaterialCallBack, *mut std::os::raw::c_void) -> *mut rpworld::RpGeometry;
+type RpClumpForAllAtomics = extern "C" fn(
+    *mut rpworld::RpClump,
+    rpworld::RpAtomicCallBack,
+    *mut std::os::raw::c_void,
+) -> *mut rpworld::RpClump;
+type RpGeometryForAllMaterials = extern "C" fn(
+    *mut rpworld::RpGeometry,
+    rpworld::RpMaterialCallBack,
+    *mut std::os::raw::c_void,
+) -> *mut rpworld::RpGeometry;
 
 pub fn rpgeometry_for_all_materials(
     geometry: *mut rpworld::RpGeometry,
@@ -24,4 +34,21 @@ pub fn rpclump_for_all_atomics(
 ) -> *mut rpworld::RpClump {
     let func: RpClumpForAllAtomics = unsafe { std::mem::transmute(0x749B70) };
     func(clump, callback, pData)
+}
+
+// TODO: generic
+pub fn set_render_state(state: u32, value: u32) {
+    let rw = unsafe { &mut (**RW_ENGINE_INSTANCE) };
+    let value = unsafe { std::mem::transmute(value) };
+    (rw.open_device.render_state_set)(state, value);
+}
+
+pub fn render_state(state: u32) -> u32 {
+    let mut value = 0;
+
+    let rw = unsafe { &mut (**RW_ENGINE_INSTANCE) };
+    let value_ref = unsafe { std::mem::transmute(&mut value) };
+    (rw.open_device.render_state_get)(state, value_ref);
+
+    value
 }
